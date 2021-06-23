ce9a6de7a3999d160b55a3d7cf9206ca1bfc8a86
andermsilva <andermarsil@gmail.com>
2021-06-23 10:19:12 -0300

    ajuste na lista de categoria

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -14,6 +14,7 @@ import { theme } from "../../global/styles/theme";
 
 type Props = RectButtonProps & {
     title: string;
+    hasCheckBox?: boolean;
     icon: React.FC<SvgProps>;
     checked?: boolean;
 }
@@ -21,10 +22,11 @@ type Props = RectButtonProps & {
 export function Category({
     title,
     icon: Icon,
+    hasCheckBox = false,
     checked = false,
     ...rest
 }: Props) {
-    const { secondary50, secondary70 } = theme.colors;
+    const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
     return (
 
         <RectButton
@@ -34,16 +36,21 @@ export function Category({
                 style={styles.container}
                 colors={[secondary50, secondary70]}
             >
-                <View style={[styles.content, { opacity: checked ? 1 : 0.4 }]}>
+                <LinearGradient
+                    style={[styles.content, { opacity: checked ? 1 : 0.5 }]}
+                    colors={[checked ? secondary85 : secondary50, secondary40]}
+                >
+                    {hasCheckBox &&
 
-                    <View style={checked ? styles.checked : styles.check} />
+                        <View style={checked ? styles.checked : styles.check} />
+                    }
                     <Icon
                         width={48}
                         height={48}
                     />
 
                     <Text style={styles.title}> {title}</Text>
-                </View>
+                </LinearGradient>
 
             </LinearGradient>
 
--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -1,4 +1,4 @@
-import { Inter_100Thin } from '@expo-google-fonts/inter';
+import { Inter_100Thin, Inter_700Bold } from '@expo-google-fonts/inter';
 import { StyleSheet } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import { theme } from '../../global/styles/theme';
@@ -17,34 +17,40 @@ export const styles = StyleSheet.create({
     content: {
         width: 100,
         height: 116,
-        backgroundColor: theme.colors.secondary40,
+
         borderRadius: 8,
         justifyContent: 'space-between',
         alignItems: 'center',
+        paddingVertical: 20
     },
 
     title: {
-        fontFamily: theme.fonts.text500,
+        fontFamily: theme.fonts.title700,
         color: theme.colors.heading,
         fontSize: 15
     },
     check: {
+        position: 'absolute',
+        top: 7,
+        right: 7,
         width: 12,
         height: 12,
         backgroundColor: theme.colors.secondary100,
-        alignSelf: 'flex-end',
-        marginRight: 7,
+
+
         borderColor: theme.colors.secondary50,
         borderWidth: 2,
         borderRadius: 3,
     },
 
     checked: {
+        position: 'absolute',
+        top: 7,
+        right: 7,
         width: 10,
         height: 10,
         backgroundColor: theme.colors.primary,
-        alignSelf: 'flex-end',
-        marginRight: 7,
+
         borderColor: theme.colors.primary,
 
         borderRadius: 3,
